621e706183b5c5b85d1ea70ff503750f1aae11df
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-01-02 22:29:16 -0600

    Added the background

---
+++ src/js/background-generate.js
@@ -0,0 +1,9 @@
+const bgContainer = document.getElementById("bg-container")
+const lines = 100
+
+for (let i = 0; i < lines; i++)
+{
+    const newDiv = document.createElement("div")
+    newDiv.classList.add("bg-line")
+    bgContainer.appendChild(newDiv)
+}
