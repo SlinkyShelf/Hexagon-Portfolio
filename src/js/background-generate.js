c6e85c675bfb9643a0a3bf0f5f095afdaf2afc62
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-01-28 21:50:02 -0600

    Reset so i can change the idea to somethign better

--- src/js/background-generate.js
+++
@@ -1,9 +0,0 @@
-const bgContainer = document.getElementById("bg-container")
-const lines = 50
-
-for (let i = 0; i < lines; i++)
-{
-    const newDiv = document.createElement("div")
-    newDiv.classList.add("bg-line")
-    bgContainer.appendChild(newDiv)
-}
